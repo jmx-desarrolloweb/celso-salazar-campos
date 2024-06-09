937595ac0e4e00e44b3ab8b256e390533e580dc1
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2024-06-08 20:13:08 -0600

    EventListener para cerrar el Navbar en mobile, añadido

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,24 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { GrMenu, GrClose } from 'react-icons/gr'
 
 export const Navbar = () => {
 
     const [openNavbar, setOpenNavbar] = useState(false)
+
+    useEffect(() => {
+        const handleHashChange = () => {
+            setOpenNavbar(false)
+        }
+
+        window.addEventListener('hashchange', handleHashChange);
+        return () => {
+            window.removeEventListener('hashchange', handleHashChange);
+        }
+    }, [])
+
+
 
     const handleCloseNavbar = () => {
         setOpenNavbar(false)
@@ -15,26 +28,28 @@ export const Navbar = () => {
         setOpenNavbar(true)
     }
 
-    
+
+
+
 
     return (
         <header className="py-1 shadow-md sticky top-0 z-10 bg-white">
             <div className="container flex items-center justify-between gap-5">
-                <figure className="w-[10rem] lg:w-[16rem] py-1">      
+                <figure className="w-[10rem] lg:w-[16rem] py-1">
                     <Image
                         src="/assets/images/logotipo_sayca.webp"
                         alt="Logo Constructora SAYCA"
                         title="Constructora SAYCA"
                         width={395}
-                        height={87}    
+                        height={87}
                     />
                 </figure>
                 <div
                     onClick={handleCloseNavbar}
-                    className={openNavbar ? "fixed inset-0 bg-neutral-950/35 z-10 md:hidden" : "hidden"} 
+                    className={openNavbar ? "fixed inset-0 bg-neutral-950/35 z-10 md:hidden" : "hidden"}
                 ></div>
-                <nav 
-                    className={`fixed left-0 top-0 bottom-0 p-10 md:p-0 bg-white shadow-lg w-[20rem] md:w-auto md:static md:block-1/2 md:bg-transparent md:shadow-none ${ openNavbar ? 'translate-x-0': 'translate-x-[-100%] md:translate-x-0' } transition-all z-10`}
+                <nav
+                    className={`fixed left-0 top-0 bottom-0 p-10 md:p-0 bg-white shadow-lg w-[20rem] md:w-auto md:static md:block-1/2 md:bg-transparent md:shadow-none ${openNavbar ? 'translate-x-0' : 'translate-x-[-100%] md:translate-x-0'} transition-all z-10`}
                 >
                     <ul className="flex flex-col md:flex-row gap-8">
                         <li>
@@ -61,7 +76,7 @@ export const Navbar = () => {
                     </button>
                 </nav>
                 <button
-                    onClick={handleOpenNavbar} 
+                    onClick={handleOpenNavbar}
                     className="border h-10 w-10 rounded-md grid place-content-center text-neutral-500 hover:bg-neutral-100 hover:text-neutral-700 transition-colors active:scale-95 md:hidden"
                 >
                     <GrMenu size={24} />
